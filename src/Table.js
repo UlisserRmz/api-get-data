e96439ebb6c33882037300fa164c452ca16e0018
UlisserRmz <ulisesrmzg20@hotmail.com>
2020-10-16 13:56:30 -0500

    Version 1.01

--- src/Table.js
+++ src/Table.js
@@ -1,45 +1,44 @@
 import React, { Component } from "react";
 import "./Table.css";
-// import { DataGrid } from "@material-ui/data-grid";
+import { DataGrid } from "@material-ui/data-grid";
 
 class Table extends Component {
   state = {
     loading: true,
-    person: null,
+    person: [],
   };
 
   async componentDidMount() {
     const response = await fetch("https://reqres.in/api/users?page=2");
     const info = await response.json();
-    this.setState({ person: info.data[0], loading: false });
+    this.setState({ person: info.data, loading: false });
+
     console.log(info.data);
+    console.log(info.data[0]["id"]);
+    console.log(info.data[0]["email"]);
   }
 
   render() {
-    // const columns = [
-    //   { field: "avatar", headerName: "Avatar", width: 120 },
-    //   { field: "id", headerName: "ID", width: 70 },
-    //   { field: "firstName", headerName: "First name", width: 130 },
-    //   { field: "lastName", headerName: "Last name", width: 130 },
-    //   { field: "email", headerName: "Email", width: 130 },
-    // ];
+    const columns = [
+      { field: "avatar", headerName: "Avatar", width: 120 },
+      { field: "id", headerName: "ID", width: 70 },
+      { field: "first_name", headerName: "First name", width: 130 },
+      { field: "last_name", headerName: "Last name", width: 130 },
+      { field: "email", headerName: "Email", width: 130 },
+    ];
 
-    // const rows = [];
-    if (this.state.loading) {
-      return console.log("Loading...");
-    } else {
-      return (
-        //   <div style={{ height: 400, width: "100%" }}>
-        //     <DataGrid
-        //       rows={rows}
-        //       columns={columns}
-        //       pageSize={5}
-        //       checkboxSelection
-        //     />
-        //   </div>
-        console.log(this.state.data)
-      );
-    }
+    const rows = this.state.person;
+    return (
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          checkboxSelection
+        />
+      </div>
+    );
   }
 }
+
 export default Table;
